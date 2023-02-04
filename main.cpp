683ff0067876114e2532c9ab105cde8d6a8f9b9a
Rishat Zagidullin <zagidullinrishat@gmail.com>
2023-02-04 21:14:53 +0300

    cmd arg added

--- main.cpp
+++ main.cpp
@@ -19,59 +19,47 @@ int main(int argc, char ** argv)
 {
     double start = get_wall_time();
 
-    int N = 80;
-    int M = 80;
+    int N = 100;
+    int M = 100;
     int K = 1;
     double dd = 0.05;
     grid_params proj_grid = grid_params(K, M, N, -0.1, -2.0, -2.0, dd);
     projection_reg proj(proj_grid);
 
-    double dt = 0.005;
-    Vector3d<int> source{0, 40, 40};
+    double dt = 0.01;
+    Vector3d<int> source{0, std::atoi(argv[1]), std::atoi(argv[2])};
     int S = 32;
     solvers::Diffusion3d_reg<double> eqn(0.1, N, M, K, dd, dt, source);
     solvers::Coagulation<double> coag(S, 1.0, dt);
 
-    int time = 0;
-    int TIME_MAX = 1000;//8000;
+    int TIME_MAX = 100;
     std::cout <<"Preprocessing time: "<<get_wall_time()-start<<"\n";
     start = get_wall_time();
 
     double * data = new double [N*M*K*S];
     double * data_new = new double [N*M*K*S];
 
-    while (true)
+    for (int time = 0; time < TIME_MAX; time++)
     {
         for (int i = 0; i < S; i++)
-        {
             eqn.iteration(data+i*N*M*K, data_new+i*N*M*K, i==0 ? 1 : 0, false);
-        }
-        for (int i = 0; i < N*M*K; i++)
-        {
-            coag.iteration(data_new+i, data+i, N*M*K);
-        }
-        //std::swap(data, data_new);
 
-        if (time % 100 == 0)
+        if(std::atoi(argv[3]) == 1)
+            for (int i = 0; i < N*M*K; i++)
+                coag.iteration(data_new+i, data+i, N*M*K);
+        else
+            std::swap(data, data_new);
+
+        for (int i = 0; i < S; i++)
         {
-            for (int i = 0; i < S; i++)
-            {
-                std::string name = std::string("./imgs/") + 
-                                   std::to_string(i) + 
-                                   std::string("_res_") + 
-                                   std::to_string(time) + 
-                                   std::string(".ppm");
-                create_ppm(N, M, data+i*N*M*K, name);
-            }
+            std::string name = std::string("./imgs/") + 
+                               std::to_string(i) + 
+                               std::string("_res_") + 
+                               std::to_string(time) + 
+                               std::string(".ppm");
+            create_ppm(N, M, data+i*N*M*K, name);
         }
 
-        if (time==TIME_MAX)
-        {
-            time=0;
-            break;
-        }
-
-        time++;
         printProgress((double)time/TIME_MAX);
     }
     std::cout <<"\nComputation time: "<< get_wall_time()-start<<"\n";
--- ppm/ppm.cpp
+++ ppm/ppm.cpp
@@ -6,40 +6,27 @@ using namespace std;
 int create_ppm(int N, int M, double const * const data, std::string filename) {
     int img_N = 256;
     int img_M = 256;
-    size_t SIZE = 256 * 256 * 3;
+    size_t SIZE = img_N * img_M;
     int * image_data = new int [SIZE];
 
     ofstream myImage;
     myImage.open(filename);
 
-    myImage << "P3" << endl;
+    myImage << "P2" << endl;
     myImage << img_N << " " << img_M << endl;
     myImage << "255" << endl;
 
     int pixel = 0;
     for (int row = 0; row < img_N; row++) {
         for (int col = 0; col < img_M; col++) {
-            int red{0};
-            int green{0};
-            int blue{0};
 
             int row_id = (int) (((float) row/ (float) img_N) * N);
             int col_id = (int) (((float) col/ (float) img_M) * M);
-            double color = data[col_id+row_id*M];
-            if (data[col_id+row_id*M] < 0.0001)
-            {
-                green = (-4./log10(color+1e-8) * 255);
-                blue = (-log10(color+1e-8)/8 * 255);
-            }
-            else
-            {
-                red = ((log10(color)/4.+1.) * 255);
-                green = (-log10(color)/4. * 255);
-            }
-            image_data[pixel*3] = red > 255 ? 255 : red;
-            image_data[pixel*3 + 1] = green > 255 ? 255 : green;
-            image_data[pixel*3 + 2] = blue > 255 ? 255 : blue; 
 
+            double val = data[col_id+row_id*M];
+            int res = (int) ( (log10(val+1e-10)+10)/10 * 255);
+
+            image_data[pixel] = res > 255 ? 255 : res;
             pixel++;
         }
     }
